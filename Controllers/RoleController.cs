a0109020a121b3cffc5ebe3fd70547a631fa890d
Pham Quang Hien <phamquanghien@Phams-MacBook-Pro.local>
2022-08-10 10:09:51 +0700

    CRUD_Role

--- Controllers/RoleController.cs
+++ Controllers/RoleController.cs
@@ -62,5 +62,69 @@ namespace DemoIdentity.Controllers
             };
             return View(model);
         }
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditRoleViewModel model)
+        {
+            var role = await _roleManager.FindByIdAsync(model.Id);
+            if (role==null)
+            {
+                ViewBag.ErrorMessage = $"Role with id = {model.Id} cannot be found.";
+                return View("NotFound");
+            }
+            else
+            {
+                role.Name = model.RoleName;
+                var result = await _roleManager.UpdateAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View(model);
+            }
+        }
+        [HttpGet]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role==null)
+            {
+                ViewBag.ErrorMessage = $"Role with id = {id} cannot be found.";
+                return View("NotFound");
+            }
+            var model = new EditRoleViewModel{
+                Id = role.Id,
+                RoleName = role.Name
+            };
+            ViewBag.roleId = role.Id;
+            ViewBag.roleName = role.Name;
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(EditRoleViewModel model)
+        {
+            var role = await _roleManager.FindByIdAsync(model.Id);
+            if (role==null)
+            {
+                ViewBag.ErrorMessage = $"Role with id = {model.Id} cannot be found.";
+                return View("NotFound");
+            }
+            else
+            {
+                var result = await _roleManager.DeleteAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View(model);
+            }
+        }
     }
 }
